989d6ed59e3e4e75a353a0f5c3336d2233b07a39
Danny Fritz <dannyfritz@gmail.com>
2019-10-31 20:22:09 -0600

    typedoc docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+/**
+ * @module flock
+ */
+
+ /**
+  * Maintains all of the entities and performs queries.
+  */
 export class World {
-  components: Map<Component<any>, (ComponentValue<any> | undefined)[]> = new Map();
-  entities: (Entity | undefined)[] = [];
-  index: number = 0;
+  private components: Map<Component<any>, (ComponentValue<any> | undefined)[]> = new Map();
+  private entities: (Entity | undefined)[] = [];
+  private index: number = 0;
   constructor () {}
   createEntity(): Entity {
     const entity = new Entity(this, this.index);
@@ -21,21 +28,21 @@ export class World {
   unregisterComponent<T>(component: Component<T>): void {
     this.components.delete(component);
   }
-  addEntityComponent<T>(entity: Entity, component: Component<T>, componentValue: ComponentValue<T>): void {
+  _addEntityComponent<T>(entity: Entity, component: Component<T>, componentValue: ComponentValue<T>): void {
     const componentValues = this.components.get(component) as ComponentValue<T>[];
     if (!componentValues) {
       throw new Error(`Component is not registered.`);
     }
     componentValues[entity.index] = componentValue;
   }
-  getEntityComponent<T>(entity: Entity, component: Component<T>): ComponentValue<T> | undefined {
+  _getEntityComponent<T>(entity: Entity, component: Component<T>): ComponentValue<T> | undefined {
     const componentValues = this.components.get(component) as (ComponentValue<T> | undefined)[];
     if (!componentValues) {
       throw new Error(`Component is not registered.`);
     }
     return componentValues[entity.index];
   }
-  removeEntityComponent<T>(entity: Entity, component: Component<T>): ComponentValue<T> | undefined {
+  _removeEntityComponent<T>(entity: Entity, component: Component<T>): ComponentValue<T> | undefined {
     const componentValues = this.components.get(component) as (ComponentValue<T> | undefined)[];
     if (!componentValues) {
       throw new Error(`Component is not registered.`);
@@ -65,11 +72,11 @@ export class World {
       }
       return componentQueries.every(cq => {
         if(cq instanceof Current) {
-          const componentValue = this.getEntityComponent(entity, cq.component);
+          const componentValue = this._getEntityComponent(entity, cq.component);
           return componentValue !== undefined;
         }
         else if(cq instanceof Without) {
-          const componentValue = this.getEntityComponent(entity, cq.component);
+          const componentValue = this._getEntityComponent(entity, cq.component);
           return componentValue === undefined;
         }
         else if(cq instanceof Added) {
@@ -94,13 +101,13 @@ export class Entity {
   }
   addComponent<T>(component: Component<T>, value?: T) {
     const componentValue = new ComponentValue(value || component.defaultValue(), component);
-    this.world.addEntityComponent(this, component, componentValue);
+    this.world._addEntityComponent(this, component, componentValue);
   }
   removeComponent<T>(component: Component<T>): void {
-    this.world.removeEntityComponent(this, component);
+    this.world._removeEntityComponent(this, component);
   }
   getComponent<T>(component: Component<T>): ComponentValue<T> | undefined {
-    return this.world.getEntityComponent(this, component);
+    return this.world._getEntityComponent(this, component);
   }
   remove() {
     this.removed = true;
@@ -124,8 +131,8 @@ export class Component<T> {
 }
 
 export class System {
-  runFunction: (...entities: Entity[][]) => void;
-  componentQueries: (ComponentQuery<any> | Component<any>)[][];
+  private runFunction: (...entities: Entity[][]) => void;
+  private componentQueries: (ComponentQuery<any> | Component<any>)[][];
   constructor(
     runFunction: (...entities: Entity[][]) => void,
     ...componentQueries: (ComponentQuery<any> | Component<any>)[][]
@@ -147,26 +154,67 @@ export class System {
   }
 }
 
+/**
+ * @ignore
+ */
 export interface ComponentQuery<T> {
   component: Component<T>;
 }
 
+/**
+ * Used to fetch all [[Entity]] that do not have a [[Component]].
+ * To be used in a [[System]] with queries.
+ *
+ * ```typescript
+ * const minorComponent = new flock.Component(() => {});
+ * const notMinor = new flock.Without(minorComponent);
+ * const watchViolentMovie = new flock.System((adults) => {}, [notMinor]);
+ * ```
+ */
 export class Without<T> implements ComponentQuery<T> {
   component: Component<T>;
   constructor(component: Component<T>) {
     this.component = component;
   }
 }
+/**
+ * Used to fetch all [[Entity]] that do have a [[Component]].
+ * To be used in a [[System]] with queries.
+ *
+ * ```typescript
+ * const minorComponent = new flock.Component(() => {});
+ * const minor = new flock.Current(minorComponent);
+ * const jumpOnBouncyCastle = new flock.System((children) => {}, [new flock.Current(minor)]);
+ * // Alternatively, because Current is very common, can be omitted.
+ * const jumpOnBouncyCastle = new flock.System((children) => {}, [minor]);
+ * ```
+ */
 export class Current<T> implements ComponentQuery<T> {
   component: Component<T>;
   constructor(component: Component<T>) {
     this.component = component;
   }
 }
+/**
+ * Used to fetch all [[Entity]] that were marked for removal since the last [[World.maintain]].
+ * To be used in a [[System]] with queries.
+ *
+ * ```typescript
+ * const funeralService = new flock.System((deadPeople) => {}, [new flock.Removed()]);
+ * ```
+ */
 export class Removed<T> implements ComponentQuery<T> {
   component: Component<T> = new Component(null as any);
   constructor() {}
 }
+/**
+ * Used to fetch all [[Entity]] that were added since the last [[World.maintain]].
+ * To be used in a [[System]] with queries.
+ *
+ * ```typescript
+ * const cry = new flock.System((babies) => {}, [new flock.Added()]);
+ * ```
+ */
 export class Added<T> implements ComponentQuery<T> {
   component: Component<T> = new Component(null as any);
   constructor() {}
