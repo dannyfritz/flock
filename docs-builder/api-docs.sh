850871a675226b92f1d5d18e561a7472da27e402
Danny Fritz <me@dannyfritz.com>
2024-09-15 20:28:24 -0500

    fresh

--- docs-builder/api-docs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ./docs/api
-typedoc --mode modules --theme ./theme --readme none --tsconfig ../src/tsconfig.json --out ./docs/api --excludePrivate --excludeNotExported --hideSources --hideBreadcrumbs --plugin typedoc-plugin-markdown ../src
-mv ./docs/api/classes/* ./docs/api/
-rm -rf ./docs/api/README.md ./docs/api/modules ./docs/api/classes
--- test/component.ts
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-import * as flock from 'flock-ecs';
-
-test('flock.Component', t => {
-  const component = new flock.Component(() => {});
-	t.truthy(component);
-});
