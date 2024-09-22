4751e0570472fc8111529bd75a5833745278ee93
Danny Fritz <me@dannyfritz.com>
2024-09-21 19:53:13 -0500

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,78 @@
-class BaseComponent { }
+class BaseComponent {}
 
 class Entity {
 	components: Map<unknown, InstanceType<typeof BaseComponent>> = new Map();
-	addComponent(component: InstanceType<typeof BaseComponent>) {
+	addComponent(component: InstanceType<typeof BaseComponent>): Entity {
+		if (this.hasComponent(component.constructor as typeof BaseComponent))
+			throw new Error(
+				`Component "${component.constructor.name}" already exists on Entity!`,
+			);
 		this.components.set(component.constructor, component);
+		return this;
 	}
-	getComponent<C extends typeof BaseComponent>(
-		Component: C,
-	): InstanceType<C> | undefined {
-		return this.components.get(Component) as InstanceType<C> | undefined;
+	getComponent<C extends typeof BaseComponent>(Component: C): InstanceType<C> {
+		const c = this.components.get(Component) as InstanceType<C>;
+		if (c === undefined)
+			throw new Error(`Component "${Component.name}" doesn't exist on Entity!`);
+		return c;
 	}
 	hasComponent(Component: typeof BaseComponent): boolean {
 		return this.components.has(Component);
 	}
-	removeComponent(Component: typeof BaseComponent) {
+	removeComponent(Component: typeof BaseComponent): Entity {
+		if (!this.hasComponent(Component))
+			throw new Error(`Component "${Component.name}" doesn't exist on Entity!`);
 		this.components.delete(Component);
-	}
-	destroy(world: World) {
-		world.removeEntity(this);
+		return this;
 	}
 }
 
 class World {
 	entities: Array<Entity> = [];
-	createEntity(): Entity {
-		const entity = new Entity();
+	addEntity(entity: Entity): World {
 		this.entities.push(entity);
-		return entity;
+		return this;
 	}
-	removeEntity(entity: Entity) {
+	removeEntity(entity: Entity): World {
 		const index = this.entities.indexOf(entity);
-		if (!index) return;
+		if (index === undefined) return this;
 		this.entities.splice(index, 1);
+		return this;
 	}
-	query(params: Array<QueryParam>): Array<Entity> {
-		return this.entities.filter((entity) => {
-			return params.every((test) => test(entity));
-		});
+	query(queryParam: QueryParam): Array<Entity> {
+		return query(this.entities, queryParam);
 	}
 }
 
+const query = (
+	entities: Array<Entity>,
+	queryParam: QueryParam,
+): Array<Entity> => {
+	return entities.filter((entity) => {
+		return queryParam(entity);
+	});
+};
+
 type QueryParam = (entity: Entity) => boolean;
 
-const With = (Component: typeof BaseComponent): QueryParam =>
-	(entity: Entity): boolean => entity.hasComponent(Component);
+const With =
+	(Component: typeof BaseComponent): QueryParam =>
+	(entity: Entity): boolean =>
+		entity.hasComponent(Component);
 
-const Without = (Component: typeof BaseComponent): QueryParam =>
-	(entity: Entity): boolean => !entity.hasComponent(Component);
+const Without =
+	(Component: typeof BaseComponent): QueryParam =>
+	(entity: Entity): boolean =>
+		!entity.hasComponent(Component);
 
-const Or = (left: QueryParam, right: QueryParam): QueryParam =>
-	(entity: Entity): boolean => left(entity) || right(entity);
+const And =
+	(...params: Array<QueryParam>): QueryParam =>
+	(entity: Entity): boolean =>
+		params.every((param) => param(entity));
 
-export { type Entity, World, Without, With, Or };
+const Or =
+	(...params: Array<QueryParam>): QueryParam =>
+	(entity: Entity): boolean =>
+		params.some((param) => param(entity));
+
+export { Entity, World, query, Without, With, And, Or };
--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,90 +1,261 @@
 import test, { describe } from "node:test";
 import assert from "node:assert";
-import { World, With } from "../src/index.ts";
+import { World, And, Or, With, Without, Entity } from "../src/index.ts";
 
 describe("flock", () => {
-	test("create world", () => {
-		const world = new World();
-	});
-
-	test("create component", () => {
-		class Component {
-			x = 0;
-		}
-		const component = new Component();
-	});
-
-	test("register component", () => {
-		class Component {
-			x = 0;
-		}
-		const world = new World();
-		const entity = world.createEntity();
-		entity.addComponent(new Component());
-	});
-
-	test("basic query", () => {
-		class Component {
-			x = 0;
-		}
-		const world = new World();
-		{
-			const result = world.query([With(Component)]);
-			assert.equal(result.length, 0);
-		}
-		{
-			const entity = world.createEntity();
+	describe("World / Entity", () => {
+		test("new World()", () => {
+			const world = new World();
+			assert.equal(world.entities.length, 0);
+		});
+		test("new Entity()", () => {
+			const entity = new Entity();
+		});
+		test("world.addEntity()", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			assert.equal(world.entities.length, 1);
+		});
+		test("entity.addComponent()", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
 			entity.addComponent(new Component());
-		}
-		{
-			const result = world.query([With(Component)]);
-			assert.equal(result.length, 1);
-			const entity = result[0];
-			const component = entity.getComponent(Component);
-			assert(component);
-			assert.equal(component.x, 0);
-		}
+			assert.equal(entity.hasComponent(Component), true);
+		});
+		test("entity.addComponent() when Component exists", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			entity.addComponent(new Component());
+			assert.throws(() => entity.addComponent(new Component()), {
+				message: 'Component "Component" already exists on Entity!',
+			});
+		});
+		test("entity.hasComponent()", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			entity.addComponent(new Component());
+			assert.equal(entity.hasComponent(Component), true);
+		});
+		test("entity.hasComponent() when Component doesn't exist", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			assert.equal(entity.hasComponent(Component), false);
+		});
+		test("entity.getComponent()", () => {
+			class Component {
+				x = 12;
+			}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			entity.addComponent(new Component());
+			assert.equal(entity.getComponent(Component)?.x, 12);
+		});
+		test("entity.getComponent() when Component doesn't exist", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			assert.throws(() => entity.getComponent(Component), {
+				message: `Component "Component" doesn't exist on Entity!`,
+			});
+		});
+		test("entity.removeComponent()", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			entity.addComponent(new Component());
+			entity.removeComponent(Component);
+			assert.equal(entity.hasComponent(Component), false);
+		});
+		test("entity.removeComponent() when Component doesn't exist", () => {
+			class Component {}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			entity.addComponent(new Component());
+			entity.removeComponent(Component);
+			assert.throws(() => entity.removeComponent(Component), {
+				message: `Component "Component" doesn't exist on Entity!`,
+			});
+		});
+		test("world.removeEntity()", () => {
+			class Component {
+				x = 0;
+			}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			world.removeEntity(entity);
+			assert.equal(world.entities.length, 0);
+		});
+		test("entity.destroy() when Entity doesn't exist", () => {
+			class Component {
+				x = 0;
+			}
+			const world = new World();
+			const entity = new Entity();
+			world.addEntity(entity);
+			world.removeEntity(entity);
+			world.removeEntity(entity);
+			assert.equal(world.entities.length, 0);
+		});
 	});
-
-	test("2 components and 2 queries", () => {
-		class ComponentA {
-			a = 0;
+	describe("Queries", () => {
+		test("And()", () => {
+			class ComponentA {}
+			class ComponentB {}
+			const world = new World();
+			assert.equal(
+				world.query(And(With(ComponentA), With(ComponentB))).length,
+				0,
+			);
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			assert.equal(
+				world.query(And(With(ComponentA), With(ComponentB))).length,
+				0,
+			);
+			world.addEntity(new Entity().addComponent(new ComponentB()));
+			assert.equal(
+				world.query(And(With(ComponentA), With(ComponentB))).length,
+				0,
+			);
+			world.addEntity(
+				new Entity()
+					.addComponent(new ComponentA())
+					.addComponent(new ComponentB()),
+			);
+			assert.equal(
+				world.query(And(With(ComponentA), With(ComponentB))).length,
+				1,
+			);
+		});
+		test("Or()", () => {
+			class ComponentA {}
+			class ComponentB {}
+			const world = new World();
+			assert.equal(
+				world.query(Or(With(ComponentA), With(ComponentB))).length,
+				0,
+			);
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			assert.equal(
+				world.query(Or(With(ComponentA), With(ComponentB))).length,
+				2,
+			);
+			world.addEntity(new Entity().addComponent(new ComponentB()));
+			assert.equal(
+				world.query(Or(With(ComponentA), With(ComponentB))).length,
+				3,
+			);
+		});
+		test("With()", () => {
+			class ComponentA {}
+			class ComponentB {}
+			const world = new World();
+			assert.equal(world.query(With(ComponentA)).length, 0);
+			assert.equal(world.query(With(ComponentB)).length, 0);
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			assert.equal(world.query(With(ComponentA)).length, 2);
+			assert.equal(world.query(With(ComponentB)).length, 0);
+			world.addEntity(new Entity().addComponent(new ComponentB()));
+			assert.equal(world.query(With(ComponentA)).length, 2);
+			assert.equal(world.query(With(ComponentB)).length, 1);
+		});
+		test("Without()", () => {
+			class ComponentA {}
+			class ComponentB {}
+			const world = new World();
+			assert.equal(world.query(Without(ComponentA)).length, 0);
+			assert.equal(world.query(Without(ComponentB)).length, 0);
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			world.addEntity(new Entity().addComponent(new ComponentA()));
+			assert.equal(world.query(Without(ComponentA)).length, 0);
+			assert.equal(world.query(Without(ComponentB)).length, 2);
+			world.addEntity(new Entity().addComponent(new ComponentB()));
+			assert.equal(world.query(Without(ComponentA)).length, 1);
+			assert.equal(world.query(Without(ComponentB)).length, 2);
+		});
+	});
+	test("All together", () => {
+		class Health {
+			hp = 0;
 		}
-		class ComponentB {
-			b = 1;
+		class Dog {
+			barks = 0;
 		}
+		class Cat {
+			meows = 0;
+		}
+		class Boss {
+			phase = 0;
+		}
+		class Player {}
 		const world = new World();
-		{
-			const result = world.query([With(ComponentA)]);
-			assert.equal(result.length, 0);
+		world.addEntity(
+			new Entity().addComponent(new Health()).addComponent(new Dog()),
+		);
+		world.addEntity(
+			new Entity().addComponent(new Health()).addComponent(new Dog()),
+		);
+		world.addEntity(
+			new Entity()
+				.addComponent(new Health())
+				.addComponent(new Dog())
+				.addComponent(new Boss()),
+		);
+		world.addEntity(
+			new Entity()
+				.addComponent(new Health())
+				.addComponent(new Cat())
+				.addComponent(new Player()),
+		);
+		let minionDogs = world.query(And(With(Dog), Without(Boss)));
+		let players = world.query(With(Player));
+		let thingsWithHealth = world.query(With(Health));
+		let bosses = world.query(With(Boss));
+		assert.equal(minionDogs.length, 2);
+		assert.equal(players.length, 1);
+		assert.equal(thingsWithHealth.length, 4);
+		assert.equal(bosses.length, 1);
+		players[0].getComponent(Health).hp = 100;
+		bosses[0].getComponent(Health).hp = 100;
+		bosses[0].getComponent(Boss).phase = 1;
+		for (const minionDog of minionDogs) {
+			minionDog.getComponent(Health).hp = 10;
 		}
-		{
-			const result = world.query([With(ComponentB)]);
-			assert.equal(result.length, 0);
-		}
-		{
-			const entity = world.createEntity();
-			entity.addComponent(new ComponentA());
-		}
-		{
-			const result = world.query([With(ComponentA)]);
-			assert.equal(result.length, 1);
-		}
-		{
-			const result = world.query([With(ComponentB)]);
-			assert.equal(result.length, 0);
-		}
-		{
-			const entity = world.createEntity();
-			entity.addComponent(new ComponentB());
-		}
-		{
-			const result = world.query([With(ComponentA)]);
-			assert.equal(result.length, 1);
-		}
-		{
-			const result = world.query([With(ComponentB)]);
-			assert.equal(result.length, 1);
-		}
+		minionDogs[1].getComponent(Health).hp = 0;
+		world.removeEntity(minionDogs[1]);
+		minionDogs = world.query(And(With(Dog), Without(Boss)));
+		players = world.query(With(Player));
+		thingsWithHealth = world.query(With(Health));
+		bosses = world.query(With(Boss));
+		assert.equal(minionDogs.length, 1);
+		assert.equal(players.length, 1);
+		assert.equal(thingsWithHealth.length, 3);
+		assert.equal(bosses.length, 1);
+		players[0].getComponent(Health).hp = 0;
+		world.removeEntity(players[0]);
+		minionDogs = world.query(And(With(Dog), Without(Boss)));
+		players = world.query(With(Player));
+		thingsWithHealth = world.query(With(Health));
+		bosses = world.query(With(Boss));
+		assert.equal(minionDogs.length, 1);
+		assert.equal(players.length, 0);
+		assert.equal(thingsWithHealth.length, 2);
+		assert.equal(bosses.length, 1);
 	});
 });
