692491c6dcdd31052be6755aa9ffef2e6eb9387f
Danny Fritz <me@dannyfritz.com>
2024-09-24 20:40:56 -0500

    remove some storybook add-ons. adjust exports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,14 @@
 import type { StorybookConfig } from "@storybook/html-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/html-vite",
-    options: {},
-  },
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: ["@storybook/docs", "@storybook/addon-interactions"],
+	framework: {
+		name: "@storybook/html-vite",
+		options: {},
+	},
+	core: {
+		disableTelemetry: true,
+	},
 };
 export default config;
