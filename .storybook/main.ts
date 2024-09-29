c20db44fdbb684944d2af661ca840ab241c8b10c
Danny Fritz <me@dannyfritz.com>
2024-09-29 15:01:41 -0500

    remove unused storybook features

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,14 @@ import type { StorybookConfig } from "@storybook/html-vite";
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: ["@storybook/addon-essentials"],
+	addons: [],
 	framework: {
 		name: "@storybook/html-vite",
 		options: {},
 	},
 	core: {
 		disableTelemetry: true,
+		disableWhatsNewNotifications: true,
 	},
 };
 export default config;
